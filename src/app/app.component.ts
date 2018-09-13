e635ba95dc2d879f198ed12fe9ace182f66e30bc
Vladislav Gulik <hulik.uladzislau@gmail.com>
2018-09-13 11:47:56 +0300

    Map operator, date pipe with format impelemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/interval';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,7 @@ import 'rxjs/add/observable/interval';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  clock = Observable.interval(1000);
+  clock = Observable
+      .interval(1000)
+      .map(() => new Date());
 }
