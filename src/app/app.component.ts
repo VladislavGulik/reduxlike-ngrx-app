3893f7765120b057fd36cfb298ccb208d6d8ce0f
Vladislav Gulik <hulik.uladzislau@gmail.com>
2018-09-13 11:44:48 +0300

    async pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  clock = Observable.interval(1000);
 }
