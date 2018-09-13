76d201d5efa552be096e6984cfc54c6b0ab257c1
Vladislav Gulik <hulik.uladzislau@gmail.com>
2018-09-13 12:18:30 +0300

    mapTo implemted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import 'rxjs/add/observable/merge';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/startWith';
 import 'rxjs/add/operator/scan';
+import 'rxjs/add/operator/mapTo';
 
 @Component({
   selector: 'app-root',
@@ -18,13 +19,21 @@ export class AppComponent {
 
   constructor() {
     this.clock = Observable.merge(
-      this.click$,
-      Observable.interval(1000)
+      this.click$.mapTo('hour'),
+      Observable.interval(1000).mapTo('second')
     )
     .startWith(new Date())
     .scan((acc, curr) => {
       const date = new Date(acc.getTime());
-      date.setSeconds(date.getSeconds() + 1);
+
+      if (curr === 'second') {
+        date.setSeconds(date.getSeconds() + 1);
+      }
+
+      if (curr === 'hour') {]
+        date.setHours(date.getHours() + 1);
+      }
+
       return date;
     });
   }
